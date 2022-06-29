d79c1cdf1d47799463c3d847a1e639642609b52e
Wallace Júnior <wflj1997@hotmail.com>
2022-06-29 01:00:10 -0300

    primeiro commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => (
+  <div className={styles.container}>
+    <h1>Hello World</h1>
+  </div>
+);
+
+export default Home;
